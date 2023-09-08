902c0285d92e25d9c1714a40dec294178cb95fe1
alex <agakinalexandr@gmail.com>
2023-09-08 14:46:25 +0200

    checking request status code

--- app/src/jivo_bot/strategies.py
+++ app/src/jivo_bot/strategies.py
@@ -1,12 +1,11 @@
-import json
 from typing import List
-
-import requests
+import httpx
+import uuid
 from abc import ABC, abstractmethod
-
 from jivo_bot.repository import AbstractRepository
 from jivo_bot.schemas import JivoRequest, ChatMessageSchema
 from config import JIVO_KEY, JIVO_URL
+from loggers import logger
 
 
 class ResponseStrategy(ABC):
@@ -37,31 +36,57 @@ class ResponseStrategy(ABC):
                 "text": message
             })
 
-    async def get_history(self, **kwargs):
-        return await self._repository.query(kwargs)
+    async def get_history(self, client_id: str, chat_id: str, channel_id: str):
+        data = {
+            "client_id": client_id,
+            "chat_id": chat_id,
+            "channel_id": channel_id
+        }
+        return await self._repository.query(data)
+
+    @staticmethod
+    async def send(url, data):
+        try:
+            async with httpx.AsyncClient() as client:
+                headers = {"Content-Type": "application/json"}
+                response = await client.post(url, json=data, headers=headers)
+                status_code = response.status_code
+                if status_code != 200:
+                    logger.error(f"Request error: {response}; url: {url}; Data: {data}")
+                return status_code
+
+        except httpx.RequestError as err:
+            logger.error(f"Request error: {err}")
+            return None
 
 
 class EchoStrategy(ResponseStrategy):
     async def send_response(self, request: JivoRequest):
-        await self.save_request(request.client_id, request.chat_id, request.channel.id, request.message.text)
-        history = await self.get_history(client_id=request.client_id, chat_id=request.chat_id,
-                                         channel_id=request.channel.id)
-        response = await self.generate_response(request, history)
+        client_id = request.client_id
+        chat_id = request.chat_id
+        channel_id = request.channel.id
+        received_text = request.message.text
+
+        await self.save_request(client_id, chat_id, channel_id, received_text)
+        history = await self.get_history(client_id, chat_id, channel_id)
+
+        response_text = await self.generate_response(request, history)
+
+        url = f"{JIVO_URL}/{JIVO_KEY}"
         data = {
-            "id": request.id,
+            "id": str(uuid.uuid4()),
             "client_id": request.client_id,
             "chat_id": request.chat_id,
             "message": {
                 "type": "TEXT",
-                "text": response,
+                "text": response_text,
             },
             "event": "BOT_MESSAGE"
         }
-        url = f"{JIVO_URL}/{JIVO_KEY}"
-        json_data = json.dumps(data)
-        # todo: check if message is sent
-        requests.post(url, data=json_data, headers={'Content-Type': 'application/json'})
-        await self.save_response(request.client_id, request.chat_id, request.channel.id, response)
+        status_code = await self.send(url, data)
+
+        if status_code == 200:
+            await self.save_response(client_id, chat_id, channel_id, response_text)
 
     async def generate_response(self, request: JivoRequest, history: List[ChatMessageSchema]) -> str:
         return f"{request.message.text!r}\n Recorded messages - {len(history)}"
